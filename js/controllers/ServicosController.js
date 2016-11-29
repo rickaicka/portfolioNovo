608aca22a4f695d7eb8408f52ca0b49fe346a813
Ricardo Salim <ricardosalim7@hotmail.com>
2016-11-29 06:52:45 -0200

    Finalização da Home

--- js/controllers/ServicosController.js
+++ js/controllers/ServicosController.js
@@ -1,7 +1,10 @@
 angular.module('Main').controller('ServicosController', function($scope){
     $scope.xList = [
         {nome:"Site Portfolio", tipo: "site", class:"site"},
-        {nome:"Sistema Controle Financeiro", tipo: "site", class:"sistema"},
-        {nome:"Sistema Controle Financeiro", tipo: "site", class:"sistema"}
+        {nome:"Sistema Controle Financeiro", tipo: "sistema", class:"sistema"},
+        {nome:"Sistema Controle Financeiro", tipo: "sistema", class:"sistema"},
+        {nome:"Sistema Controle Financeiro", tipo: "sistema", class:"sistema"},
+        {nome:"Sistema Controle Financeiro", tipo: "sistema", class:"sistema"},
+        {nome:"Sistema Controle Financeiro", tipo: "sistema", class:"sistema"}
     ];
 })
