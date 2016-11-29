ce1403c59117a790b4825521bb902bb91ee4dfdc
Ricardo Salim <ricardosalim7@hotmail.com>
2016-11-28 22:23:41 -0200

    Arquivos necessários

---
+++ js/controllers/ServicosController.js
@@ -0,0 +1,7 @@
+angular.module('Main').controller('ServicosController', function($scope){
+    $scope.xList = [
+        {nome:"Site Portfolio", tipo: "site", class:"site"},
+        {nome:"Sistema Controle Financeiro", tipo: "site", class:"sistema"},
+        {nome:"Sistema Controle Financeiro", tipo: "site", class:"sistema"}
+    ];
+})
---
+++ js/main.js
@@ -0,0 +1,14 @@
+angular.module('Main', ['ngRoute',  'iso.directives']).config(function($routeProvider){
+
+    $routeProvider.when('/index', {
+        templateUrl:'partials/home.html',
+        controller:'IndexController'
+    });
+
+    $routeProvider.when('/', {
+        templateUrl:'partials/home.html',
+        controller:'IndexController'
+    });
+
+    $routeProvider.otherwise({redirectTo:'/'})
+});
