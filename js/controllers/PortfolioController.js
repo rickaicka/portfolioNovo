0a7d3e538b53ed847563acce27c5de0028e5820f
Ricardo Salim <ricardosalim7@hotmail.com>
2016-11-30 01:50:07 -0200

    Correção no Back-End

--- js/controllers/PortfolioController.js
+++ js/controllers/PortfolioController.js
@@ -1,14 +1,22 @@
-angular.module('Main').controller('PortfolioController', function($scope, $http){
+angular.module('Main').controller('PortfolioController', function($scope, $resource){
     
     $scope.cases = [];
+    $scope.filtro = '';
+    var Case = $resource('/portfolio/:id');
     
-    $http.get('/portfolio')
-    .success(function(data){
-        $scope.cases = data;
-    })
-    .error(function(statusText){
-        console.log("Não foi possível obter lista de Cases do Portfolio");
-        console.log(statusText);
-    })
+    $scope.init = function(){
+        buscaCases();
+    };
     
+    function buscaCases(){
+        Case.query(
+            function(cases){
+                $scope.cases = cases;
+            }, function(erro){
+                console.log("Não foi possível obter lista de cases");
+                console.log(erro);
+            }
+        );
+    };
+    $scope.init();
 })
--- js/main.js
+++ js/main.js
@@ -1,4 +1,4 @@
-angular.module('Main', ['ngRoute',  'iso.directives']).config(function($routeProvider){
+angular.module('Main', ['ngRoute', 'iso.directives', 'ngResource']).config(function($routeProvider){
 
     $routeProvider.when('/index', {
         templateUrl:'partials/home.html',
